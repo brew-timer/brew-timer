8b855228352b53d03ecd6933e5cff385628fd8b1
IlyaBurch <ilyaburch@yandex.ru>
2023-11-01 14:33:56 +0300

    still trying to deploy

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/frontend/" : "/",
-  };
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/frontend/'
+      : '/'
+  }
